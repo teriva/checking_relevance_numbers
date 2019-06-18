77b91fa9667a93380fe6c1eacbdc2f1a79b8e056
Константин Спирин <>
2019-06-18 06:29:20 +0300

    add the get_all_number method

--- main.py
+++ main.py
@@ -28,3 +28,18 @@ def check_relevance_numbers(input_data):
         pool.map(check, input_data)
     print("total time spent: ", time.time() - time_start_test)
 
+def get_numbers(url):
+    parse = parser.parse(url, HEADERS)
+    if len(parse) != 0:
+        print("test", url, "result=OK time=" + parse[1], "numbers:")
+        for number in parse[0]:
+            print(number)
+    else:
+        print("test", url, "result=NUMBERS DON'T MATCH!!!")
+
+
+def get_all_numbers(urls):
+    time_start_test = time.time()
+    with Pool(NUMBER_PROCESSES) as pool:
+        pool.map(get_numbers, urls)
+    print("total time spent: ", time.time() - time_start_test)
